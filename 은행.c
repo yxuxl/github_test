89e65baa1d2f43fcdcb6295aaf791032a5947b8c
yxuxl <crystal333999@naver.com>
2024-10-25 17:05:49 +0900

    Add files via upload

---
+++ 은행.c
@@ -0,0 +1,13 @@
+﻿'은행다시.c.exe'(Win32): 'C:\Users\김유리\Desktop\C언어\은행다시.c\x64\Debug\은행다시.c.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\ntdll.dll'을(를) 로드했습니다. 
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\kernel32.dll'을(를) 로드했습니다. 
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\KernelBase.dll'을(를) 로드했습니다. 
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\apphelp.dll'을(를) 로드했습니다. 
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\vcruntime140d.dll'을(를) 로드했습니다. 
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\ucrtbased.dll'을(를) 로드했습니다. 
+9944 스레드가 종료되었습니다(코드: 0 (0x0)).
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\kernel.appcore.dll'을(를) 로드했습니다. 
+'은행다시.c.exe'(Win32): 'C:\Windows\System32\msvcrt.dll'을(를) 로드했습니다. 
+13628 스레드가 종료되었습니다(코드: 0 (0x0)).
+26756 스레드가 종료되었습니다(코드: 0 (0x0)).
+'[14936] 은행다시.c.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
