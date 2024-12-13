875b2227403d17461b4641f72aa741eebe5fda2e
yxuxl <crystal333999@naver.com>
2024-12-13 17:06:38 +0900

    기말_최대최소총합.c

---
+++ 기말_최대최소총합.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[5];
+	int max, min, sum, i;
+	for (i = 0;i < 5;i++)
+	{
+		printf("입력: ");
+		scanf("%d", &arr[i]);
+	}
+	max = min = sum = arr[0];
+	for (i = 1;i < 5;i++)
+	{
+		sum += arr[i];
+		if (max < arr[i])
+			max = arr[i];
+		if (min > arr[i])
+			min = arr[i];
+	}
+	printf("최댓값: %d\n", max);
+	printf("최솟값: %d\n", min);
+	printf("총합: %d\n", sum);
+	
+	return 0;
+}
